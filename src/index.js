15d23596f8566013aa09a6cc20a13ed01ec520cf
enkhjino <96076106+enkhjino@users.noreply.github.com>
2022-04-19 14:40:55 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function charCount(str) {
+  var obj = {};
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i].toLowerCase();
+    if (/[a-z0-9]/.test(char)) {
+      if (obj[char] > 0) {
+      }
+    }
+  }
+}
